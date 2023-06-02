f8e9aa4fa6e1fa513ba8b949e4fa064677a54be2
Boneflether <135040523+Boneflether@users.noreply.github.com>
2023-06-02 03:00:59 -0300

    Update pilha.h

--- Questao 2/pilha.h
+++ Questao 2/pilha.h
@@ -1,90 +1,90 @@
 //Questão 2 - a
-struct Node{
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
     int item;
     struct Node *prox;
 };
 typedef struct Node node;
 
-int vazia(node *PILHA){
-    if(PILHA->prox == NULL)
+int vazia(node *PILHA) {
+    if (PILHA->prox == NULL)
         return 1;
-   else
+    else
         return 0;
 }
 
 void empilha(node *PILHA) {
-    node *novo=(node *) malloc(sizeof(node));
+    node *novo = (node *)malloc(sizeof(node));
     novo->prox = NULL;
-    
+
     printf("Digite o novo item: ");
     scanf("%d", &novo->item);
-    
-    if(vazia(PILHA))
+
+    if (vazia(PILHA))
         PILHA->prox = novo;
-    else{
+    else {
         node *tmp = PILHA->prox;
-        while(tmp->prox != NULL){
+        while (tmp->prox != NULL) {
             tmp = tmp->prox;
         }
         tmp->prox = novo;
     }
-    tam++;
 }
 
-void imprime(node *PILHA){
-    if(vazia(PILHA)){
+void imprimePilha(node *PILHA) {
+    if (vazia(PILHA)) {
         printf("PILHA vazia!\n\n");
-    return;
+        return;
     }
     node *tmp;
     tmp = PILHA->prox;
-    printf("\nPILHA:");
-    while(tmp != NULL){
-        printf("%3d", tmp->item);
+    printf("PILHA:");
+    while (tmp != NULL) {
+        printf("%3d ", tmp->item);
         tmp = tmp->prox;
     }
-    printf("\nQtde itens PILHA: %d", tam);
     printf("\n");
 }
 
-void imprimeTopo(node *PILHA){
-    if(vazia(PILHA)){
+void imprimeTopo(node *PILHA) {
+    if (vazia(PILHA)) {
         printf("PILHA vazia!\n\n");
-    return;
+        return;
     }
     node *tmp;
     tmp = PILHA->prox;
-    while(tmp != NULL){
-        if(temp->prox == NULL){
-            printf("topo: %3d", tmp->item);
-        }else{
-            tmp = tmp->prox;
+    while (tmp != NULL) {
+        if (tmp->prox == NULL) {
+            printf("topo: %3d\n", tmp->item);
         }
+        tmp = tmp->prox;
+        
     }
-    printf("\nQtde itens PILHA: %d", tam);
-    printf("\n");
+
 }
 
-void desempilha(node *PILHA){
-    if(PILHA->prox == NULL){
+void desempilha(node *PILHA) {
+    if (PILHA->prox == NULL) {
         printf("PILHA ja vazia\n\n");
         return;
-    }else{
+    }
+    else {
         node *ultimo = PILHA->prox;
         node *penultimo = PILHA;
-        
-    while(ultimo->prox != NULL){
-        penultimo = ultimo;
-        ultimo = ultimo->prox;
+
+        while (ultimo->prox != NULL) {
+            penultimo = ultimo;
+            ultimo = ultimo->prox;
         }
         free(ultimo);
         penultimo->prox = NULL;
-        tam--;
     }
 }
 
-void limparFila(node *PILHA){
-    while (!vazia(PILHA)){
+void limparPilha(node *PILHA) {
+    while (!vazia(PILHA)) {
         desempilha(PILHA);
     }
 }
